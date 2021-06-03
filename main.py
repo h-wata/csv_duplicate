7fb0856ee22979db607973d0f8a9599587eea39f
h-wata <wata.hirofumi@gmail.com>
2021-06-03 16:48:55 +0900

    remove comment

--- main.py
+++ main.py
@@ -10,12 +10,9 @@ pd.set_option('display.max_columns', None)
 for csv_file in csv_files: 
     print("File Name : {}".format(csv_file))
     data = pd.read_csv(csv_file, encoding='cp932', header=None)
-    # print(data)
 
     print("重複業の抽出を実行します")
-    # is_duplicated = data.duplicated(subset=[0, 1, 3, 20, 21])
     is_duplicated = data.duplicated(subset=[0, 1, 3, 5, 7, 8, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22])
-    # print(is_duplicated)
     print(data[is_duplicated])
 
 var = input("Please Input Enter")
